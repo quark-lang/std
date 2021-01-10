b9d6e29c2b89e3e2aa202e9ff45f4f90c2903fe2
thomasvergne <thomas@quark-lang.dev>
2021-01-10 14:17:29 +0100

    🐛 STD functions fixed.

--- std.ts
+++ std.ts
@@ -1,15 +1,32 @@
 import { QuarkModule } from '../api/api.ts';
 import { QuarkTypes } from '../api/typings/types.ts';
+import type { ValueElement } from '../src/core/interpreter.ts';
+import { Types } from '../src/core/interpreter.ts';
 
 QuarkModule.declare('std', QuarkTypes.QuarkFunction, {
   name: 'out',
-  body: async function(text: string) {
-    const encodedText: Uint8Array = (new TextEncoder).encode(text);
-    await Deno.stdout.write(encodedText);
+  body: async function(text: ValueElement) {
+    if ('value' in text) {
+      const encodedText: Uint8Array = (new TextEncoder).encode(String(text.value));
+      await Deno.stdout.write(encodedText);
+    }
   }
 });
 
 QuarkModule.declare(null, QuarkTypes.QuarkVariable, {
   name: 'newline',
-  value: '\n',
+  value: {
+    type: Types.String,
+    value: '\n',
+  },
+});
+
+QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
+  name: 'type',
+  body: function(variable: any): ValueElement {
+    return {
+      type: Types.String,
+      value: typeof variable.value,
+    };
+  }
 });
