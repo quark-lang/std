a35b9511bfcf07924f533ee33777ce1676ea2f13
thomasvergne <thomas@quark-lang.dev>
2021-01-12 13:26:15 +0100

    ✨ FS API converted to new API.

--- fs.ts
+++ fs.ts
@@ -1,31 +1,48 @@
-import { Interpreter } from '../src/core/interpreter.ts';
-import * as path from 'https://deno.land/std@0.83.0/path/mod.ts';
 import { File } from '../src/utils/file.ts';
+import { QuarkModule } from '../api/api.ts';
+import { QuarkTypes } from '../api/typings/types.ts';
+import { StringType, Types } from '../src/core/interpreter.ts';
+import * as path from 'https://deno.land/std@0.83.0/path/mod.ts';
 
-export const module = [
-  {
-    name: 'read',
-    func: async function(path: string) {
-      try {
-        const content: string = await File.read(path);
-        return content;
-      } catch (error) {
-        throw error;
+QuarkModule.declare('fs', QuarkTypes.QuarkFunction, {
+  name: 'read',
+  body: async function(path: StringType): Promise<StringType> {
+    try {
+      const content: string = await File.read(path.value);
+      return {
+        type: Types.String,
+        value: content,
       }
+    } catch (error) {
+      throw error;
     }
-  },
-  {
-    name: 'cwd',
+  }
+});
+
+QuarkModule.declare('fs', QuarkTypes.QuarkVariable, {
+  name: 'cwd',
+  value: {
+    type: Types.String,
     value: Deno.cwd(),
   },
-  {
-    name: 'join',
-    func: path.join,
-  },
-  {
-    name: 'dir',
-    func: path.dirname,
-  }
-];
+});
 
-export const namespace = 'fs';
+QuarkModule.declare('fs', QuarkTypes.QuarkFunction, {
+  name: 'join',
+  body: function(...paths: StringType[]): StringType {
+    return {
+      type: Types.String,
+      value: path.join(...paths.map((x) => x.value)),
+    };
+  }
+});
+
+QuarkModule.declare('fs', QuarkTypes.QuarkFunction, {
+  name: 'dirname',
+  body: function(src: StringType): StringType {
+    return {
+      type: Types.String,
+      value: path.dirname(src.value),
+    }
+  }
+});
