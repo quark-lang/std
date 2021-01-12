3f0147a1a035695f6d82a2c884a7f44714a3405d
thomasvergne <thomas@quark-lang.dev>
2021-01-12 16:47:31 +0100

    ✨ Fetch module added.

---
+++ fetch.ts
@@ -0,0 +1,18 @@
+import { QuarkModule } from '../api/api.ts';
+import { QuarkTypes } from '../api/typings/types.ts';
+import { StringType, Types } from '../src/core/interpreter.ts';
+
+QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
+  name: 'fetch',
+  body: async function(path: StringType): Promise<StringType> {
+    try {
+      const res = await fetch(path.value);
+      return {
+        type: Types.String,
+        value: await res.text(),
+      }
+    } catch (exception) {
+      throw exception;
+    }
+  }
+});
