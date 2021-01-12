d37e5b500be25616f66da7d8989f83ad7b589446
thomasvergne <thomas@quark-lang.dev>
2021-01-12 13:54:56 +0100

    ✨ Input and code execution functions added.

--- std.ts
+++ std.ts
@@ -1,7 +1,8 @@
 import { QuarkModule } from '../api/api.ts';
 import { QuarkTypes } from '../api/typings/types.ts';
-import type { ValueElement } from '../src/core/interpreter.ts';
-import { Types } from '../src/core/interpreter.ts';
+import type { StringType, ValueElement } from '../src/core/interpreter.ts';
+import { Interpreter, Types } from '../src/core/interpreter.ts';
+import { Parser } from '../src/core/parser.ts';
 
 QuarkModule.declare('std', QuarkTypes.QuarkFunction, {
   name: 'out',
@@ -21,6 +22,14 @@ QuarkModule.declare(null, QuarkTypes.QuarkVariable, {
   },
 });
 
+QuarkModule.declare('std', QuarkTypes.QuarkVariable, {
+  name: 'args',
+  value: {
+    type: Types.List,
+    value: Deno.args.map((acc) => ({ type: Types.String, value: acc })),
+  },
+});
+
 QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
   name: 'type',
   body: function(variable: any): ValueElement {
@@ -32,9 +41,38 @@ QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
 });
 
 QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
+  name: 'input',
+  body: async function(question: StringType): Promise<StringType> {
+    const buf = new Uint8Array(1024);
+    await Deno.stdout.write(new TextEncoder().encode(question.value));
+
+    const input = await Deno.stdin.read(buf);
+    const answer = new TextDecoder().decode(buf.subarray(0, input as number | undefined));
+    return {
+      type: Types.String,
+      value: answer.trim(),
+    };
+  }
+});
+
+QuarkModule.declare('std', QuarkTypes.QuarkFunction, {
+  name: 'exec',
+  body: async function(code: StringType): Promise<StringType> {
+    return await Interpreter.process(Parser.parse(code.value));
+  }
+});
+
+QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
   name: 'throw',
   body: function(message: ValueElement) {
     if (!message) return;
     throw 'value' in message ? message.value : '';
+  }
+});
+
+QuarkModule.declare('std', QuarkTypes.QuarkFunction, {
+  name: 'run',
+  body: async function(code: StringType, path: StringType) {
+    await Interpreter.run(code.value, path.value);
   }
 });
