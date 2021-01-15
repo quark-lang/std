1979d44cb9f6b986e37c8fa6887acbc9ddab2bbf
thomasvergne <thomas@quark-lang.dev>
2021-01-15 22:51:26 +0100

    🐛 REPL Version fallback fixed.

--- quark.ts
+++ quark.ts
@@ -5,7 +5,7 @@ import { Types } from '../src/core/interpreter.ts';
 async function getRelease(): Promise<string> {
   const github = await fetch('https://api.github.com/repos/quark-lang/quark/releases');
   const json = await github.json();
-
+  if (json.message && json.message.includes('API rate limit')) return 'v1.0.0';
   return json[0].tag_name || 'v1.0.0';
 }
 
