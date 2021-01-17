42f7c786a0978021ac89b32327186438222d624b
NessMC <contact@nessmc.fr>
2021-01-17 16:28:13 +0100

    🐛 Read path return none if not exists fixed.

--- core/fs.ts
+++ core/fs.ts
@@ -1,20 +1,20 @@
 import { File } from '../../src/utils/file.ts';
 import { QuarkModule } from '../../api/api.ts';
 import { QuarkTypes } from '../../api/typings/types.ts';
-import { StringType, Types } from '../../src/core/interpreter.ts';
+import { existsSync } from 'https://deno.land/std/fs/mod.ts';
+import {NoneType, StringType, Types} from '../../src/core/interpreter.ts';
 import * as path from 'https://deno.land/std@0.83.0/path/mod.ts';
 
 QuarkModule.declare('fs', QuarkTypes.QuarkFunction, {
   name: 'read',
-  body: async function(path: StringType): Promise<StringType> {
-    try {
-      const content: string = await File.read(path.value);
-      return {
-        type: Types.String,
-        value: content,
-      }
-    } catch (error) {
-      throw error;
+  body: async function(path: StringType): Promise<StringType | NoneType> {
+    if (!existsSync(path.value)) return {
+      type: Types.None,
+      value: undefined,
+    }
+    return {
+      type: Types.String,
+      value: await File.read(path.value),
     }
   }
 });
