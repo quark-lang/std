f01f4fb0d03097fb0e09dbbbf87320bf56968dfc
thomasvergne <thomas@quark-lang.dev>
2021-01-18 14:56:13 +0100

    🐛 Release getter fallback fixed.

--- quark.ts
+++ quark.ts
@@ -1,12 +1,17 @@
 import { QuarkModule } from '../api/api.ts';
 import { QuarkTypes } from '../api/typings/types.ts';
 import { FunctionType, Types, Function } from '../src/core/interpreter.ts';
+import { parseConfiguration, getQuarkFolder } from '../src/main.ts';
+import * as Path from 'https://deno.land/std@0.83.0/path/mod.ts';
 
 async function getRelease(): Promise<string> {
   const github = await fetch('https://api.github.com/repos/quark-lang/quark/releases');
   const json = await github.json();
-  if (json.message && json.message.includes('API rate limit')) return 'v1.0.0';
-  return json[0].tag_name || 'v1.0.0';
+  const path: string = Path.join(await getQuarkFolder(), '.quarkrc');
+  const config = await parseConfiguration(path);
+
+  if (json.message && json.message.includes('API rate limit')) return config['version'];
+  return json[0].tag_name || config['version'];
 }
 
 QuarkModule.declare('quark', QuarkTypes.QuarkVariable, {
