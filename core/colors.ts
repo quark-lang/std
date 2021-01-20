12359001a8f0b5f8499d05d41f4e17c54269d8b3
thomasvergne <thomas@quark-lang.dev>
2021-01-20 12:21:01 +0100

    ♻️ Colors functions replaced with quarkify API.

--- core/colors.ts
+++ core/colors.ts
@@ -1,65 +1,40 @@
 import { quarkify, QuarkModule } from '../../api/api.ts';
 import { QuarkTypes } from '../../api/typings/types.ts';
-import { StringType, Types, ValueElement } from '../../src/core/interpreter.ts';
+import { StringType } from '../../src/core/interpreter.ts';
 import { bold, green, red, rgb24, yellow } from 'https://deno.land/std@0.83.0/fmt/colors.ts';
 
 // green
 QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
   name: 'green',
-  body: function(message: StringType) {
-    return {
-      type: Types.String,
-      value: green(message.value),
-    };
-  }
+  body: (message: StringType) => quarkify(green, message),
 });
 
 // gray
 QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
   name: 'gray',
-  body: function(message: StringType) {
-    return {
-      type: Types.String,
-      value: rgb24(message.value, 0x808080),
-    };
-  }
+  body: (message: StringType) => quarkify(rgb24, message, 0x808080),
 });
 
 // yellow
 QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
   name: 'yellow',
-  body: (...args: ValueElement[]) => quarkify(yellow, ...args)
+  body: (message: StringType) => quarkify(yellow, message),
 });
 
 // white
 QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
   name: 'white',
-  body: function(message: StringType) {
-    return {
-      type: Types.String,
-      value: rgb24(message.value, 0xffffff),
-    }
-  }
+  body: (message: StringType) => quarkify(rgb24, message, 0xffffff),
 });
 
 // red
 QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
   name: 'red',
-  body: function(message: StringType) {
-    return {
-      type: Types.String,
-      value: red(message.value),
-    };
-  }
+  body: (message: StringType) => quarkify(red, message),
 });
 
 // bold
 QuarkModule.declare(null, QuarkTypes.QuarkFunction, {
   name: 'bold',
-  body: function(message: StringType) {
-    return {
-      type: Types.String,
-      value: bold(message.value),
-    }
-  }
+  body: (message: StringType) => quarkify(bold, message),
 });
